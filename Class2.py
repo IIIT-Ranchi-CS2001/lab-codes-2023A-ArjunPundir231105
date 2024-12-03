cf8ae4cd83428d5c886f2385e8b269dc8df57dff
ArjunPundir231105 <148452492+ArjunPundir231105@users.noreply.github.com>
2024-12-03 15:15:32 +0530

    Add files via upload

---
+++ Class2.py
@@ -0,0 +1,16 @@
+# Fibonacci series using while loop
+n = int(input("Enter the number of terms: "))
+
+
+a, b = 0, 1
+i = 0
+
+if n == 1:
+    print(f"Fibonacci sequence up to {n}:")
+    print(a)
+else:
+    print("Fibonacci sequence:")
+    while i < n:
+        print(a, end=" ")
+        a, b = b, a + b
+        i += 1
